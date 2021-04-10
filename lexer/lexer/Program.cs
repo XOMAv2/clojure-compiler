a69dab7c4b1a3acaf01bcc2cd7a357aaf2e9ef38
Firova Draia <d.firo@yandex.ru>
2021-04-10 18:57:08 +0300

    Lexer and Parser are generated by ANTLR4.

---
+++ lexer/lexer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Antlr4.Runtime;
+
+namespace lexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input expression.");
+        
+                // to type the EOF character and end the input: use CTRL+D, then press <enter>
+                string input = Console.ReadLine();
+
+                AntlrInputStream inputStream = new AntlrInputStream(input);
+                ClojureLexer speakLexer = new ClojureLexer(inputStream);
+                CommonTokenStream commonTokenStream = new CommonTokenStream(speakLexer);
+                Console.WriteLine(commonTokenStream.GetNumberOfOnChannelTokens());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
