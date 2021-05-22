4966509622c4230bc379a7cdcbf8892a184e82f7
Дарья Фирова <d.v.firova@gmail.com>
2021-05-22 22:56:40 +0300

    Added walker.

---
+++ lexer/lexer/Implementation/ClojureWalker.cs
@@ -0,0 +1,474 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace lexer.Implementation
+{
+    class ClojureWalker : IClojureListener
+    {
+        ParseTreeProperty<XElement> xml = new ParseTreeProperty<XElement>();
+
+        public XElement Root { get; private set; }
+        XElement GetXml(IParseTree ctx)
+        {
+            return xml.Get(ctx);
+        }
+
+        XElement GetParentXml(IParseTree ctx)
+        {
+            var parent = ctx.Parent;
+
+            XElement result = GetXml(parent);
+
+            if (result == null)
+                result = GetParentXml(parent);
+
+            return result;
+        }
+
+        private void AddValue(IParseTree context, string value)
+        {
+            var parent = GetParentXml(context);
+            parent.Value = value;
+        }
+
+        private void AddElement(IParseTree context, string name)
+        {
+            XElement element = new XElement(name);
+            XElement ParentElement = GetParentXml(context);
+            ParentElement.Add(element);
+            SetXml(context, element);
+        }
+
+        void SetXml(IParseTree ctx, XElement e)
+        {
+            xml.Put(ctx, e);
+        }
+        public void EnterAny_char([NotNull] ClojureParser.Any_charContext context)
+        {
+
+            AddElement(context, "any_char");
+        }
+
+        public void EnterBacktick([NotNull] ClojureParser.BacktickContext context)
+        {
+            AddElement(context, "backtick");
+        }
+
+        public void EnterBign([NotNull] ClojureParser.BignContext context)
+        {
+            AddElement(context, "bign");
+        }
+
+        public void EnterBin([NotNull] ClojureParser.BinContext context)
+        {
+            AddElement(context, "bin");
+        }
+
+        public void EnterCharacter([NotNull] ClojureParser.CharacterContext context)
+        {
+            AddElement(context, "character");
+        }
+
+        public void EnterDeref([NotNull] ClojureParser.DerefContext context)
+        {
+            AddElement(context, "deref");
+        }
+
+        public void EnterDiscard([NotNull] ClojureParser.DiscardContext context)
+        {
+            AddElement(context, "discard");
+        }
+
+        public void EnterDispatch([NotNull] ClojureParser.DispatchContext context)
+        {
+            AddElement(context, "dispatch");
+        }
+
+        public void EnterEveryRule(ParserRuleContext ctx)
+        {
+            
+        }
+
+        public void EnterFile([NotNull] ClojureParser.FileContext context)
+        {
+            Root = new XElement("file");
+            SetXml(context, Root);
+        }
+
+        public void EnterForm([NotNull] ClojureParser.FormContext context)
+        {
+            AddElement(context, "form");
+        }
+
+        public void EnterForms([NotNull] ClojureParser.FormsContext context)
+        {
+            AddElement(context, "forms");
+        }
+
+        public void EnterGensym([NotNull] ClojureParser.GensymContext context)
+        {
+            AddElement(context, "gensym");
+        }
+
+        public void EnterHex([NotNull] ClojureParser.HexContext context)
+        {
+            AddElement(context, "hex");
+        }
+
+        public void EnterHost_expr([NotNull] ClojureParser.Host_exprContext context)
+        {
+            AddElement(context, "host_expr");
+        }
+
+        public void EnterKeyword([NotNull] ClojureParser.KeywordContext context)
+        {
+            AddElement(context, "keyword");
+        }
+
+        public void EnterLambda([NotNull] ClojureParser.LambdaContext context)
+        {
+            AddElement(context, "lambda");
+        }
+
+        public void EnterList([NotNull] ClojureParser.ListContext context)
+        {
+            AddElement(context, "list");
+        }
+
+        public void EnterLiteral([NotNull] ClojureParser.LiteralContext context)
+        {
+            AddElement(context, "literal");
+        }
+
+        public void EnterMacro_keyword([NotNull] ClojureParser.Macro_keywordContext context)
+        {
+            AddElement(context, "macro_keyword");
+        }
+
+        public void EnterMap([NotNull] ClojureParser.MapContext context)
+        {
+            AddElement(context, "map");
+        }
+
+        public void EnterMeta_data([NotNull] ClojureParser.Meta_dataContext context)
+        {
+            AddElement(context, "meta_data");
+        }
+
+        public void EnterNamed_char([NotNull] ClojureParser.Named_charContext context)
+        {
+            AddElement(context, "named_char");
+        }
+
+        public void EnterNil([NotNull] ClojureParser.NilContext context)
+        {
+            AddElement(context, "nil");
+        }
+
+        public void EnterNs_symbol([NotNull] ClojureParser.Ns_symbolContext context)
+        {
+            AddElement(context, "ns_symbol");
+        }
+
+        public void EnterNumber([NotNull] ClojureParser.NumberContext context)
+        {
+            AddElement(context, "number");
+        }
+
+        public void EnterParam_name([NotNull] ClojureParser.Param_nameContext context)
+        {
+            AddElement(context, "param_name");
+        }
+
+        public void EnterQuote([NotNull] ClojureParser.QuoteContext context)
+        {
+            AddElement(context, "quote");
+        }
+
+        public void EnterReader_macro([NotNull] ClojureParser.Reader_macroContext context)
+        {
+            AddElement(context, "reader_macro");
+        }
+
+        public void EnterRegex([NotNull] ClojureParser.RegexContext context)
+        {
+            AddElement(context, "regex");
+        }
+
+        public void EnterSet([NotNull] ClojureParser.SetContext context)
+        {
+            AddElement(context, "set");
+        }
+
+        public void EnterSimple_keyword([NotNull] ClojureParser.Simple_keywordContext context)
+        {
+            AddElement(context, "simple_keyword");
+        }
+
+        public void EnterSimple_sym([NotNull] ClojureParser.Simple_symContext context)
+        {
+            AddElement(context, "simple_sym");
+        }
+
+        public void EnterString([NotNull] ClojureParser.StringContext context)
+        {
+            AddElement(context, "string");
+        }
+
+        public void EnterSymbol([NotNull] ClojureParser.SymbolContext context)
+        {
+            AddElement(context, "symbol");
+        }
+
+        public void EnterTag([NotNull] ClojureParser.TagContext context)
+        {
+            AddElement(context, "tag");
+        }
+
+        public void EnterUnquote([NotNull] ClojureParser.UnquoteContext context)
+        {
+            AddElement(context, "unquote");
+        }
+
+        public void EnterUnquote_splicing([NotNull] ClojureParser.Unquote_splicingContext context)
+        {
+            AddElement(context, "unquote_splicing");
+        }
+
+        public void EnterU_hex_quad([NotNull] ClojureParser.U_hex_quadContext context)
+        {
+            AddElement(context, "u_hex_quad");
+        }
+
+        public void EnterVar_quote([NotNull] ClojureParser.Var_quoteContext context)
+        {
+            AddElement(context, "var_quote");
+        }
+
+        public void EnterVector([NotNull] ClojureParser.VectorContext context)
+        {
+            AddElement(context, "vector");
+        }
+
+        public void ExitAny_char([NotNull] ClojureParser.Any_charContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitBacktick([NotNull] ClojureParser.BacktickContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitBign([NotNull] ClojureParser.BignContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitBin([NotNull] ClojureParser.BinContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitCharacter([NotNull] ClojureParser.CharacterContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitDeref([NotNull] ClojureParser.DerefContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitDiscard([NotNull] ClojureParser.DiscardContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitDispatch([NotNull] ClojureParser.DispatchContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitEveryRule(ParserRuleContext ctx)
+        {
+            
+        }
+
+        public void ExitFile([NotNull] ClojureParser.FileContext context)
+        {
+            XElement file = GetXml(context);
+            file.Value = context.Start.Text;
+        }
+
+        public void ExitForm([NotNull] ClojureParser.FormContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitForms([NotNull] ClojureParser.FormsContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitGensym([NotNull] ClojureParser.GensymContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitHex([NotNull] ClojureParser.HexContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitHost_expr([NotNull] ClojureParser.Host_exprContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitKeyword([NotNull] ClojureParser.KeywordContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitLambda([NotNull] ClojureParser.LambdaContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitList([NotNull] ClojureParser.ListContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitLiteral([NotNull] ClojureParser.LiteralContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitMacro_keyword([NotNull] ClojureParser.Macro_keywordContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitMap([NotNull] ClojureParser.MapContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitMeta_data([NotNull] ClojureParser.Meta_dataContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitNamed_char([NotNull] ClojureParser.Named_charContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitNil([NotNull] ClojureParser.NilContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitNs_symbol([NotNull] ClojureParser.Ns_symbolContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitNumber([NotNull] ClojureParser.NumberContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitParam_name([NotNull] ClojureParser.Param_nameContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitQuote([NotNull] ClojureParser.QuoteContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitReader_macro([NotNull] ClojureParser.Reader_macroContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitRegex([NotNull] ClojureParser.RegexContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitSet([NotNull] ClojureParser.SetContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitSimple_keyword([NotNull] ClojureParser.Simple_keywordContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitSimple_sym([NotNull] ClojureParser.Simple_symContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitString([NotNull] ClojureParser.StringContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitSymbol([NotNull] ClojureParser.SymbolContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitTag([NotNull] ClojureParser.TagContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitUnquote([NotNull] ClojureParser.UnquoteContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitUnquote_splicing([NotNull] ClojureParser.Unquote_splicingContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitU_hex_quad([NotNull] ClojureParser.U_hex_quadContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitVar_quote([NotNull] ClojureParser.Var_quoteContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void ExitVector([NotNull] ClojureParser.VectorContext context)
+        {
+            AddValue(context, context.Start.Text);
+        }
+
+        public void VisitErrorNode(IErrorNode node)
+        {
+            Console.WriteLine("Visiting ErrorNode");
+        }
+
+        public void VisitTerminal(ITerminalNode node)
+        {
+            Console.WriteLine("Visiting Terminal");
+        }
+    }
+}
--- lexer/lexer/Program.cs
+++ lexer/lexer/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using lexer.Implementation;
 
 namespace lexer
 {
@@ -10,27 +14,45 @@ namespace lexer
         {
             try
             {
-                Console.Write("Input expression: ");
-                string input = Console.ReadLine();
+                //Console.Write("Input expression: ");
+                string input = "{:a 1, :b 2}";//Console.ReadLine();
                 AntlrInputStream inputStream = new AntlrInputStream(input);
                 ClojureLexer lexer = new ClojureLexer(inputStream);
                 CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-                IList<IToken> tokens = new List<IToken>();
-                while (commonTokenStream.LA(1) != -1)
+                ClojureParser parser = new ClojureParser(commonTokenStream);
+                IParseTree tree = parser.file();
+                ParseTreeWalker walker = new ParseTreeWalker();
+                ClojureWalker clojureWalker = new ClojureWalker();
+                walker.Walk(clojureWalker, tree);
+                XElement root = clojureWalker.Root;
+                using (StringWriter sw = new StringWriter())
                 {
-                    tokens.Add(commonTokenStream.LT(1));
-                    commonTokenStream.Consume();
+                    foreach(XElement elem in root.Elements())
+                    {
+                        Console.WriteLine(elem.ToString());
+                    }
                 }
+                    //ClojureParser.CharacterContext context = parser.character();
+                    //ClojureBaseVisitor<String> visitor = new ClojureBaseVisitor<string>();
+                    //var fl = ClojureParser.FLOAT
+                    //visitor.Visit(context);
 
-                int n = commonTokenStream.GetNumberOfOnChannelTokens() - 1;
-                Console.WriteLine("Number tokens: " + n);
-                int k = 1;
-                foreach (IToken tok in tokens)
-                {
-                    Console.WriteLine(k + " token: " + tok.Text);
-                    k++;
+                    /*IList<IToken> tokens = new List<IToken>();
+                    while (commonTokenStream.LA(1) != -1)
+                    {
+                        tokens.Add(commonTokenStream.LT(1));
+                        commonTokenStream.Consume();
+                    }
+
+                    int n = commonTokenStream.GetNumberOfOnChannelTokens() - 1;
+                    Console.WriteLine("Number tokens: " + n);
+                    int k = 1;
+                    foreach (IToken tok in tokens)
+                    {
+                        Console.WriteLine(k + " token: " + tok.Text);
+                        k++;
+                    }*/
                 }
-            }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex);
