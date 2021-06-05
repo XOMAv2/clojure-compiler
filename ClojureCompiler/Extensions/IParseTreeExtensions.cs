5d20122456f3e10c41cbcf34c5b6e7e1362f634f
Nikita Iksaritsa <xomav2@gmail.com>
2021-06-05 15:00:28 +0300

    Метод для печати IParseTree вынесен в класс расширений

---
+++ ClojureCompiler/Extensions/IParseTreeExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace ClojureCompiler.Extensions
+{
+    public static class IParseTreeExtensions
+    {
+        public static string ToIndentedTree(this IParseTree tree, Parser parser)
+        {
+            StringBuilder buf = new();
+            ToIndentedRecursive(tree, buf, 0, parser.RuleNames.ToList());
+
+            return buf.ToString();
+        }
+
+        private static void ToIndentedRecursive(IParseTree node, StringBuilder buf, int offset, List<string> ruleNames)
+        {
+            for (int i = 0; i < offset; i++)
+            {
+                buf.Append("  ");
+            }
+
+            buf.Append(Trees.GetNodeText(node, ruleNames)).Append('\n');
+
+            if (node is ParserRuleContext prc)
+            {
+                if (prc.children != null)
+                {
+                    foreach (IParseTree child in prc.children)
+                    {
+                        ToIndentedRecursive(child, buf, offset + 1, ruleNames);
+                    }
+                }
+            }
+        }
+    }
+}
--- ClojureCompiler/Program.cs
+++ ClojureCompiler/Program.cs
@@ -1,46 +1,13 @@
 ﻿using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using ClojureCompiler.Generated;
+using ClojureCompiler.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ClojureCompiler
 {
     class Program
     {
-        public static string PrintSyntaxTree(Parser parser, IParseTree root)
-        {
-            StringBuilder buf = new();
-            Recursive(root, buf, 0, parser.RuleNames.ToList());
-
-            return buf.ToString();
-        }
-
-        private static void Recursive(IParseTree aRoot, StringBuilder buf, int offset, List<string> ruleNames)
-        {
-            for (int i = 0; i < offset; i++)
-            {
-                buf.Append("  ");
-            }
-
-            buf.Append(Trees.GetNodeText(aRoot, ruleNames)).Append('\n');
-
-            if (aRoot is ParserRuleContext)
-            {
-                ParserRuleContext prc = (ParserRuleContext)aRoot;
-
-                if (prc.children != null)
-                {
-                    foreach (IParseTree child in prc.children)
-                    {
-                        Recursive(child, buf, offset + 1, ruleNames);
-                    }
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
             try
@@ -66,14 +33,12 @@ namespace ClojureCompiler
                 CommonTokenStream tokenStream = new(lexer);
                 ClojureParser parser = new(tokenStream);
                 IParseTree root = parser.file();
-                Console.WriteLine(PrintSyntaxTree(parser, root));
+                Console.WriteLine(root.ToIndentedTree(parser));
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error: " + e);
             }
-
-            Console.ReadLine();
         }
     }
 }
