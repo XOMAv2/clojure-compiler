ab64087dc6a05af0f587596220384b76a51a2d4c
Nikki <iksaritsaNI@tsniimash.ru>
2021-06-09 16:08:21 +0300

    Добавлены новые исключения

---
+++ ClojureCompiler/Exceptions/ArityException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClojureCompiler.Exceptions
+{
+    /// <summary>
+    /// Thrown during parsing if the language rules are not satisfied.
+    /// </summary>
+    [Serializable]
+    public class ArityException : Exception
+    {
+        public ArityException()
+        { }
+
+        public ArityException(string message)
+            : base(message)
+        { }
+
+        public ArityException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ ClojureCompiler/Exceptions/TypeCastException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClojureCompiler.Exceptions
+{
+    /// <summary>
+    /// Thrown during parsing if the language rules are not satisfied.
+    /// </summary>
+    [Serializable]
+    public class TypeCastException : Exception
+    {
+        public TypeCastException()
+        { }
+
+        public TypeCastException(string message)
+            : base(message)
+        { }
+
+        public TypeCastException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
