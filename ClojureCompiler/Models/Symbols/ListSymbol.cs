763b004e5bc59484736fae9eebdbd3f2aa4ff41b
Nikita Iksaritsa <xomav2@gmail.com>
2021-06-14 15:46:48 +0300

    Добавлены классы VectorSymbol и ListSymbol

---
+++ ClojureCompiler/Models/Symbols/ListSymbol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Antlr4.Runtime;
+using static ClojureCompiler.Generated.ClojureParser;
+
+namespace ClojureCompiler.Models.Symbols
+{
+    public class ListSymbol : CollectionSymbol
+    {
+        public ListSymbol(
+            SymbolContext context,
+            Scope scope,
+            Dictionary<string, ParserRuleContext> meta = null)
+            : base(context, scope, meta)
+        { }
+
+        public ListSymbol(string name) : base(name)
+        { }
+
+        public ListSymbol(string name, Scope scope) : base(name, scope)
+        { }
+    }
+}
---
+++ ClojureCompiler/Models/Symbols/VectorSymbol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Antlr4.Runtime;
+using static ClojureCompiler.Generated.ClojureParser;
+
+namespace ClojureCompiler.Models.Symbols
+{
+    public class VectorSymbol : CollectionSymbol
+    {
+        public VectorSymbol(
+            SymbolContext context,
+            Scope scope,
+            Dictionary<string, ParserRuleContext> meta = null)
+            : base(context, scope, meta)
+        { }
+
+        public VectorSymbol(string name) : base(name)
+        { }
+
+        public VectorSymbol(string name, Scope scope) : base(name, scope)
+        { }
+    }
+}
